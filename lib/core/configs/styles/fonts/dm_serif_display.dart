25ce152a05905bed7795595ea0247fb0f4b32f5d
Roy P A <roy@nintrivalabs.com>
2025-01-16 15:29:38 +0530

    fix: ui bugs fixed

--- lib/core/configs/styles/fonts/dm_serif_display.dart
+++ lib/core/configs/styles/fonts/dm_serif_display.dart
@@ -2,32 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../app_colors.dart';
 
-
 class DmSerifDisplayPalette {
   static const String dmSerifDisplayFamily = "DMSerifDisplay";
 
+  /*<---15--->*/
+  static TextStyle get fWhite_18_400 => TextStyle(
+      fontSize: 18.sp, fontWeight: FontWeight.w400, color: AppColors.white);
+
   /*<---24--->*/
   static TextStyle get fWhite_24_400 => TextStyle(
-      fontSize: 24.sp,
-      fontWeight: FontWeight.w400,
-      color: AppColors.white);
+      fontSize: 24.sp, fontWeight: FontWeight.w400, color: AppColors.white);
 
 /*<---26--->*/
   static TextStyle get ffefefe_26_400 => TextStyle(
-      fontSize: 26.sp,
-      fontWeight: FontWeight.w400,
-      color: AppColors.white);
-
-  /*<---32--->*/
-  static TextStyle get fWhite_32_400 => TextStyle(
-      fontSize: 32.sp,
-      fontWeight: FontWeight.w400,
-      color: AppColors.white);
+      fontSize: 26.sp, fontWeight: FontWeight.w400, color: AppColors.white);
 
   /*<---40--->*/
-  static TextStyle get fdfdfdf_40_400 => TextStyle(
+  static TextStyle get fWhite_60_400 => TextStyle(
       height: 1,
-      fontSize: 40.sp,
+      fontSize: 60.sp,
       fontWeight: FontWeight.w400,
       color: AppColors.white);
 }
--- lib/features/main/presentation/view/main_screen.dart
+++ lib/features/main/presentation/view/main_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:lottie/lottie.dart';
 
+import '../../../../core/configs/constants/app_json.dart';
 import '../../../../core/configs/constants/string_constants.dart';
 import '../../../../core/configs/styles/app_colors.dart';
+import '../../../../core/configs/styles/fonts/dm_serif_display.dart';
 import '../bloc/main_bloc.dart';
 
 class MainScreen extends StatelessWidget {
@@ -12,8 +15,24 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.backgroundColor,
-      body: SizedBox(
+      appBar: AppBar(
+        leading: const SizedBox(),
+        backgroundColor: AppColors.transparent,
+        actions: [
+          GestureDetector(
+            onTap: () {},
+            child: Icon(
+              Icons.replay,
+              color: AppColors.white,
+              size: 30.w,
+            ),
+          ),
+          25.horizontalSpace
+        ],
+      ),
+      backgroundColor: AppColors.black,
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 10.h, horizontal: 10.w),
         width: double.maxFinite,
         height: double.maxFinite,
         child: BlocConsumer<MainBloc, MainState>(
@@ -23,14 +42,82 @@ class MainScreen extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Icon(
-                    state is FlashlightOnState
-                        ? Icons.flashlight_on
-                        : Icons.flashlight_off,
-                    color: AppColors.ffd8b0e,
-                    size: 80.w,
+                  Expanded(
+                    flex: 1,
+                    child: Column(
+                      children: [
+                        Icon(
+                          Icons.location_pin,
+                          color: AppColors.white,
+                          size: 30.w,
+                        ),
+                        Text("India",
+                            style: DmSerifDisplayPalette.fWhite_24_400),
+                        Text("Date",
+                            style: DmSerifDisplayPalette.fWhite_18_400),
+                      ],
+                    ),
                   ),
-                  35.verticalSpace,
+                  40.verticalSpace,
+                  Text("22", style: DmSerifDisplayPalette.fWhite_60_400),
+                  25.verticalSpace,
+                  Lottie.asset(AppJson.jsonThunder,
+                      repeat: true,
+                      reverse: false,
+                      height: 150.w,
+                      width: 150.w,
+                      animate: true, onLoaded: (composition) {
+                    debugPrint(composition.duration.toString());
+                  }, delegates: const LottieDelegates()),
+                  25.verticalSpace,
+                  Expanded(
+                    flex: 2,
+                    child: SizedBox(
+                      width: 250.w,
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Expanded(
+                                flex: 5,
+                                child: Text("Humidity (%)",
+                                    style: DmSerifDisplayPalette.fWhite_18_400),
+                              ),
+                              Expanded(
+                                child: Text(" - ",
+                                    style: DmSerifDisplayPalette.fWhite_18_400),
+                              ),
+                              Expanded(
+                                flex: 2,
+                                child: Text("sa ",
+                                    style: DmSerifDisplayPalette.fWhite_18_400),
+                              )
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Expanded(
+                                flex: 5,
+                                child: Text("Wind Speed (m/s)",
+                                    style: DmSerifDisplayPalette.fWhite_18_400),
+                              ),
+                              Expanded(
+                                child: Text(" - ",
+                                    style: DmSerifDisplayPalette.fWhite_18_400),
+                              ),
+                              Expanded(
+                                flex: 2,
+                                child: Text("sas",
+                                    style: DmSerifDisplayPalette.fWhite_18_400),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
                 ],
               );
             }),
